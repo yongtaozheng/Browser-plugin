dc0a3695f96633fcd38da362e57979a5eb61da3d
JYeontu <yongtaozheng0201@163.com>
2021-12-29 11:41:21 +0800

    更新configDemo.js

--- Chrome-GitLab-plugin/configDemo.js
+++ Chrome-GitLab-plugin/configDemo.js
@@ -1,6 +1,17 @@
+//输入框快捷跳转配置
 var searchConfig = {
-    baseUrl:"https://blog.csdn.net/Twinkle_sone?spm=1001.2100.3001.5343",
-    is:"https://blog.csdn.net/Twinkle_sone?spm=1001.2100.3001.5343",
-    mr:"https://blog.csdn.net/Twinkle_sone?spm=1001.2100.3001.5343",
-    jk:"https://blog.csdn.net/Twinkle_sone?spm=1001.2100.3001.5343"
+    baseUrl:"https://gitee.com/",//搜索框为空时跳转
+    csdn:"https://blog.csdn.net/Twinkle_sone?spm=1001.2100.3001.5343",
+    gitee:"https://gitee.com/",
+    juejin:"https://juejin.cn/",
+}
+//全局快捷键配置
+var shortcutsKeys = {
+    //打开面板
+    open:{//默认alt+z
+        ctrlKey:false,//是否按住ctrl
+        altKey:true,//是否按住alt
+        shiftKey:false,//是否按住shift
+        keyCode:keyBoardKeyConfig['v']//其他键
+    }
 }
